41e32b4b2427f959a82eedb3d18b81fa9356959d
Daniel Kornas <harvi_pl@wp.pl>
2019-02-03 21:17:06 +0100

    App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('check render App and if form elements exist', () => {
+  test('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  test('includes form', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('input')).toHaveLength(4);
+    expect(wrapper.find('button')).toHaveLength(1);
+  });
+});
+
+describe('check input values onChange', () => {
+  const wrapper = mount(<App />);
+  const testValue = 'Text';
+  const inputUsername = wrapper.find('input[name="username"]');
+  const inputEmail = wrapper.find('input[name="email"]');
+  const inputPassword = wrapper.find('input[name="password"]');
+  const inputAccept = wrapper.find('input[name="accept"]');
+
+  beforeEach(() => { 
+    inputUsername.instance().value = testValue;
+    inputUsername.simulate('change');
+
+    inputEmail.instance().value = testValue;
+    inputEmail.simulate('change');
+
+    inputPassword.instance().value = testValue;
+    inputPassword.simulate('change');
+
+    inputAccept.instance().checked= true;
+    inputAccept.simulate('change');
+    
+  });
+
+  test('check input username', () => {
+    expect(wrapper.state().username).toEqual(testValue);
+    expect(wrapper.state().email).toEqual(testValue);
+    expect(wrapper.state().password).toEqual(testValue);
+    expect(wrapper.state().accept).toEqual(true);
+  });
+
+  test('check input email', () => {
+    expect(wrapper.state().email).toEqual(testValue);
+  });
+
+  test('check input password', () => {
+    expect(wrapper.state().password).toEqual(testValue);
+  });
+
+  test('check input checkbox', () => {
+    expect(wrapper.state().accept).toEqual(true);
+  });
 });
