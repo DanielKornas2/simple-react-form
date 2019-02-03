3c8a0e24c9aefd66f630eba23e5713629d4788d0
Daniel Kornas <harvi_pl@wp.pl>
2019-02-03 12:29:29 +0100

    State and object with warnings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    accept: false,
+    message: '',
+
+    errors: {
+      username: false,
+      email: false,
+      password: false,
+      accept: false,
+    }
+  }
+
+  warnings = {
+    username_incorrect: "Nazwa nie może zawierać spacji i musi być dłuższa niż 2 litery",
+    email_incorrect: "Email musi zawierać @",
+    password_incorrect: "Hasło musi być dłuższe niż 5 znaków",
+    accept_incorrect: "Zakceptuj regulamin.",
+  }
+  
   render() {
     return (
       <div className="App">
