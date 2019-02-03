3b9d31ac55631cae4cd5dc0e2ddc6b810fbabf1e
Daniel Kornas <harvi_pl@wp.pl>
2019-02-03 12:59:27 +0100

    HandleSubmit, HandleChange and form validation in app

--- src/App.js
+++ src/App.js
@@ -25,6 +25,105 @@ class App extends Component {
     accept_incorrect: "Zakceptuj regulamin.",
   }
 
+  handleChange = (e) => {
+    // name = odczytuję name z inputa, np. email i na tej podstawie ustawiam stan
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "checkbox") {
+      const isChecked = e.target.checked;
+
+      // uzywam nawiasow kwadratowych bo przekazuje zmienna jako parametr
+      this.setState({
+       [name]: isChecked,
+      })
+    } else {
+      this.setState({
+        [name]: e.target.value,
+      })
+    }
+    
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validation = this.formValidation();
+
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        password: "",
+        accept: false,
+        message: 'Formularz został wysłany!',
+    
+        errors: {
+          username: false,
+          email: false,
+          password: false,
+          accept: false,
+        }
+      }) 
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          password: !validation.password,
+          accept: !validation.accept,
+        }
+      })
+    }
+  }
+
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if (this.state.username.length >= 2 && this.state.username.indexOf(' ') === -1) {
+      username = true;
+    }
+
+    if (this.state.email.indexOf('@') !== -1) {
+      email = true;
+    }
+
+    if (this.state.password.length >= 6) {
+      password = true;
+    }
+
+    if (this.state.accept) {
+      accept = true;
+    }
+
+    if (username && email && password && accept){
+      correct = true;
+    }
+
+    return ({
+      username,
+      email,
+      password,
+      accept,
+      correct
+    })
+
+  }
+
+  componentDidUpdate(){
+    // po 4 sekundach od wysłania kasuję wiadomość o wysłaniu formularza, czyli zeruje message w state
+    if(this.state.message !== ''){
+      setTimeout(() => 
+      this.setState({
+        message: '',
+      }), 4000)
+    }
+  }
+
   render() {
     return (
       <div className="App">
