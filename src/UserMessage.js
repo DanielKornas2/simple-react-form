0a5e8bb010708dc420633d116c55061527423e65
Daniel Kornas <harvi_pl@wp.pl>
2019-02-28 17:18:16 +0100

    React Hooks - testing

--- src/UserMessage.js
+++ src/UserMessage.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // w tym komponencie tylko odczytuję stan ze store'a 
 import { connect } from "react-redux";
 
-const UserMessage = (props) => (
-    <ul>
+const UserMessage = (props) => {
+    const [clickValue, clickHandler] = useState(false);
+
+    useEffect(()=>{
+
+    })
+
+    return (
+        <ul>
         <li><strong>Imię:</strong> {props.userState.username}</li>
         <li><strong>Email:</strong> {props.userState.email}</li>
-    </ul>
-)
+        <li onClick={()=> clickHandler(clickValue===true)}>Click me</li>
+        </ul>
+    )
+}
+    
+
 
 //Nie używam mapDispatchProps - nie potrzebuję tu żadnej akcji, tylko odczytuję dane ze store'a
 const mapStateToProps = state => {
@@ -17,6 +28,6 @@ const mapStateToProps = state => {
         // nazwa właściwości jest dowolna - taką jak wpiszę potem używam jako props - redux przekazuje stan jako props
         userState: state.myStore
     };
-  };
+};
 
 export default connect(mapStateToProps)(UserMessage);
